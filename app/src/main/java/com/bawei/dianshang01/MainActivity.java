83b122d445cfcb8c673087bc7c44e69befc5aaf2
Liyize97126 <1216431704@qq.com>
2020-03-20 23:33:36 +0800

    0320-MVC架构(方法2)

--- app/src/main/java/com/bawei/dianshang01/MainActivity.java
+++ app/src/main/java/com/bawei/dianshang01/MainActivity.java
@@ -2,14 +2,24 @@ package com.bawei.dianshang01;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     //定义
     private EditText wenbenshow;
-    private Handler handler;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if(msg.obj != null){
+                wenbenshow.setText((String) msg.obj);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,19 +29,15 @@ public class MainActivity extends AppCompatActivity {
         //wenbenshow.setCursorVisible(false);//隐藏光标
         //wenbenshow.setFocusable(false);//失去焦点
         //wenbenshow.setFocusableInTouchMode(false);//虚拟键盘隐藏
-        handler = new Handler();
         //网络请求框架    Volley、OKHttp、Retrofit
         new Thread(new Runnable() {
             @Override
             public void run() {
-                //发送请求
-                final PengPaiListBean instance = NewsModel.getInstance();
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        wenbenshow.setText(instance.code + "\n" + instance.listdata.get(1).content);
-                    }
-                });
+                String json = NetUtil.getNetUtil().doGet("http://blog.zhaoliang5156.cn/api/pengpainews/pengpai1.json");
+                //发送消息
+                Message message = Message.obtain();
+                message.obj = json;
+                handler.sendMessage(message);
             }
         }).start();
     }
